b17970ce6b25fa6da1fa1f1c3fa8613d6e210081
emilsabev2003 <150591482+emilsabev2003@users.noreply.github.com>
2024-05-20 19:40:54 -0700

    added game music and added all other audio

--- src/scenes/MenuScene.js
+++ src/scenes/MenuScene.js
@@ -4,17 +4,29 @@ class MenuScene extends Phaser.Scene {
     }
 
     preload() {
+        //load image/audio assets
         this.load.image("menuBackground", "./assets/mainmenu.png")
+        this.load.audio('gameMusic', './assets/the_big_heist.wav')
+
     }
 
     create() {
+
+        //add menu background image 
         this.backgroundImage = this.add.tileSprite(0, 0, 912, 720, "menuBackground").setOrigin(0, 0)
         this.startGame = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.P)
+
+        this.music = this.sound.add('gameMusic')
+        this.music.setVolume(0.3)
+
+
     }
 
     update() {
+        //check for keyboard input P to start game
         if (this.startGame.isDown)
         {
+            this.music.play({loop: true})
             this.scene.start("PlayScene")
         }
             
