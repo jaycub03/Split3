37d1c5e53fcacdc15540116f5b404b04b4a6a465
emilsabev2003 <150591482+emilsabev2003@users.noreply.github.com>
2024-05-20 22:15:02 -0700

    game over scene working

--- src/scenes/GameOverScene.js
+++ src/scenes/GameOverScene.js
@@ -4,15 +4,22 @@ class GameOverScene extends Phaser.Scene {
     }
 
     preload() {
-
+        //load credits background
+        this.load.image("goBackground", "./assets/credits.png")
     }
 
     create() {
-
+        //add credits background image 
+        this.backgroundImage = this.add.tileSprite(0, 0, 912, 720, "goBackground").setOrigin(0, 0)
+        this.startOverGame = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.P)
     }
 
     update() {
-
+        //check for keyboard input P to start over game
+        if (this.startOverGame.isDown)
+            {
+                this.scene.start("PlayScene")
+            }
     }
 
 }
